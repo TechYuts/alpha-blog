e31f96d74bef48811e208573dabc6f6b693b55cc
EC2 Default User <--get>
2019-02-01 02:35:14 +0000

    add many to many association category and article

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_23_024634) do
+ActiveRecord::Schema.define(version: 2019_02_01_021618) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
